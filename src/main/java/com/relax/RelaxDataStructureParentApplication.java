ffb78e1f56d1e250911ba49e7a5b59d65bffd323
_relax <ahaaaa04@163.com>
2024-08-16 09:46:39 +0800

    init project via upload

---
+++ src/main/java/com/relax/RelaxDataStructureParentApplication.java
@@ -0,0 +1,13 @@
+package com.relax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxDataStructureParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelaxDataStructureParentApplication.class, args);
+    }
+
+}
